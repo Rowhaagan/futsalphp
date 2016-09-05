af1eba602f4d0e817f274dd5d3ac81cbb600674a
Rowhaagan <haronmaharjan@gmail.com>
2016-09-05 22:55:50 +0545

    Add files via upload

--- admin/uploads.php
+++ admin/uploads.php
@@ -1,17 +1,49 @@
 <?php
-$ds = DIRECTORY_SEPARATOR;  // Store directory separator (DIRECTORY_SEPARATOR) to a simple variable. This is just a personal preference as we hate to type long variable name.
- 
-$storeFolder = 'uploads';   // Declare a variable for destination folder.
+
+include('../config/connection.php');
+
+$ds = DIRECTORY_SEPARATOR;  //1
+$id = $_GET['id'];
+
+$storeFolder = '../uploads';   //2
+
+$ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+$newname = time();
+$random = rand(100,999);
+$name = $newname.$random.'.'.$ext;
+
+// $query = "SELECT avatar FROM users WHERE user_id = $id";
+// $result = mysqli_query($conn, $query);
+// $old = mysqli_fetch_assoc($result);
+
+
+$query = "UPDATE users SET avatar = '$name' WHERE user_id = $id";
+$result = mysqli_query($conn, $query);
+
+echo $query.'<br>';
+echo mysqli_error($conn);
  
 if (!empty($_FILES)) {
      
-    $tempFile = $_FILES['file']['tmp_name'];          // If file is sent to the page, store the file object to a temporary variable.            
+    $tempFile = $_FILES['file']['tmp_name'];          //3             
       
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  // Create the absolute path of the destination folder.
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
      
-    $targetFile =  $targetPath. $_FILES['file']['name'];  // Create the absolute path of the uploaded file destination.
+    $targetFile =  $targetPath. $name;  //5
  
-    move_uploaded_file($tempFile,$targetFile); // Move uploaded file to destination.
-     
+    move_uploaded_file($tempFile,$targetFile); //6    
+    
+  //   $deleteFile = $targetPath.$old['avatar'];
+	
+  //   if($old['avatar'] != '') {
+    	
+		// if(!is_dir($deleteFile)) {
+			
+		// 	unlink($deleteFile);
+			
+		// }
+
+    // }
+  
 }
 ?> 
